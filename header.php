4c9999271cf2f81979fb7525fa2e07e7e2c4b572
root <root@DESKTOP-GGRKVNT.localdomain>
2023-02-02 14:50:33 -0500

    Testing

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="UTF-8">
+	<title>Drone Detection </title>
+		<link href="monarchstd.css" rel="stylesheet">   <!-- Stylesheet link -->
+		<link href="stylesheet.css" rel="stylesheet"> <!-- Stylesheet link -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+define ('SITE_ROOT', realpath(dirname(__FILE__)));
+/*Defines all HTML standards and sets stylesheets ^ */
+
+
+?>
+
+<?php
+
+
+  /*Checks if user is logged in through the previous session.  */
+
+  /*Login button redirects to login page*/
+
+
+	/*IF the user is not logged in they will be directed to the login page. Login is required*/
+
+
+?>
+  </head>
---
+++ home.php
@@ -0,0 +1,18 @@
+<?php $page = "index";
+?>
+<?php include_once("header.php");
+?>
+<?php include_once("menu.php");
+?>
+
+
+
+
+  <!-- Home Body -->
+
+
+
+
+
+<?php include_once("footer.php");
+?>
